7ffdcde5c60363af02bd9a5cd5f1afccca11b566
Artem V. Navrotskiy <bozaro@users.noreply.github.com>
2015-10-02 21:57:12 +0300

    Add test for replaying object download

--- gitlfs-client/src/test/java/ru/bozaro/gitlfs/client/ClientLegacyTest.java
+++ gitlfs-client/src/test/java/ru/bozaro/gitlfs/client/ClientLegacyTest.java
@@ -1,8 +1,11 @@
 package ru.bozaro.gitlfs.client;
 
+import com.google.common.io.ByteStreams;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Replay tests for https://github.com/github/git-lfs/blob/master/docs/api/http-v1-original.md
@@ -18,4 +21,11 @@ public class ClientLegacyTest {
     client.putObject(new StringStreamProvider("Fri Oct 02 21:07:33 MSK 2015"));
   }
 
+  @Test
+  public void legacyDownload01() throws IOException {
+    final HttpReplay replay = YamlHelper.createReplay("/ru/bozaro/gitlfs/client/legacy-download-01.yml");
+    final Client client = new Client(new FakeAuthProvider(), replay);
+    final byte[] data = ByteStreams.toByteArray(client.getObject("b810bbe954d51e380f395de0c301a0a42d16f115453f2feb4188ca9f7189074e"));
+    Assert.assertEquals(new String(data, StandardCharsets.UTF_8), "Fri Oct 02 21:07:33 MSK 2015");
+  }
 }
