git-as-svn/git-lfs-java

commit 00d07252a0cf2fba1f08dcecf78b9add680a286f

Update gradle & dependencies

gitlfs-common/src/main/java/ru/bozaro/gitlfs/common/JsonHelper.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -24,7 +24,7 @@ public final class JsonHelper {
     final ObjectMapper mapper = new ObjectMapper();
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     mapper.enable(SerializationFeature.INDENT_OUTPUT);
-    mapper.setDateFormat(new ISO8601DateFormat());
+    mapper.setDateFormat(new StdDateFormat());
     mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
     return mapper;
   }

build.gradle
@@ -1,16 +1,17 @@
-task wrapper(type: Wrapper) {
-  gradleVersion = "2.11"
+wrapper {
+    gradleVersion = '5.3.1'
 }
 
 def javaVersion = JavaVersion.VERSION_1_8
 
 buildscript {
   repositories {
+    mavenCentral()
     jcenter()
   }
 
   dependencies {
-    classpath "com.github.ben-manes:gradle-versions-plugin:0.12.0"
+    classpath "com.github.ben-manes:gradle-versions-plugin:0.21.0"
   }
 }
 
@@ -40,10 +41,10 @@ subprojects {
   ext.ossrhPassword = System.getenv("OSSRH_PASSWORD") ?: ""
 
   dependencies {
-    compile "org.jetbrains:annotations:15.0"
+    compile "org.jetbrains:annotations:17.0.0"
 
-    testCompile "com.google.guava:guava:19.0"
-    testCompile "org.testng:testng:6.9.10"
+    testCompile "com.google.guava:guava:27.1-jre"
+    testCompile "org.testng:testng:6.14.3"
   }
 
   idea {

gitlfs-client/build.gradle
@@ -2,9 +2,9 @@ description = "Java Git-LFS client library"
 
 dependencies {
   compile project(":gitlfs-common")
-  compile "org.apache.httpcomponents:httpclient:4.5.1"
-  compile "org.slf4j:slf4j-api:1.7.13"
+  compile "org.apache.httpcomponents:httpclient:4.5.8"
+  compile "org.slf4j:slf4j-api:1.7.26"
 
-  testCompile "org.yaml:snakeyaml:1.16"
-  testCompile "org.slf4j:slf4j-simple:1.7.13"
+  testCompile "org.yaml:snakeyaml:1.24"
+  testCompile "org.slf4j:slf4j-simple:1.7.26"
 }

gitlfs-client/src/test/java/ru/bozaro/gitlfs/client/YamlRepresenter.java
@@ -24,10 +24,10 @@ public class YamlRepresenter extends Representer {
     public Node representData(Object data) {
       byte[] value = (byte[]) data;
       if (Utf8.isWellFormed(value)) {
-        return representScalar(new Tag("!text"), new String(value, StandardCharsets.UTF_8), DumperOptions.ScalarStyle.LITERAL.getChar());
+        return representScalar(new Tag("!text"), new String(value, StandardCharsets.UTF_8), DumperOptions.ScalarStyle.LITERAL);
       } else {
         String binary = Base64Coder.encodeLines((byte[]) data);
-        return representScalar(Tag.BINARY, binary, '|');
+        return representScalar(Tag.BINARY, binary, DumperOptions.ScalarStyle.LITERAL);
       }
     }
   }

gitlfs-common/build.gradle
@@ -1,6 +1,6 @@
 description = "Java Git-LFS common structures"
 
 dependencies {
-  compile "com.fasterxml.jackson.core:jackson-databind:2.7.0"
-  compile "com.google.guava:guava:19.0"
+  compile "com.fasterxml.jackson.core:jackson-databind:2.9.8"
+  compile "com.google.guava:guava:27.1-jre"
 }

gitlfs-server/build.gradle
@@ -2,9 +2,9 @@ description = "Java Git-LFS server implementation-free library"
 
 dependencies {
   compile project(":gitlfs-common")
-  compile "javax.servlet:javax.servlet-api:3.1.0"
+  compile "javax.servlet:javax.servlet-api:4.0.1"
 
   testCompile project(":gitlfs-client")
-  testCompile "org.eclipse.jetty:jetty-server:9.3.7.RC1"
-  testCompile "org.eclipse.jetty:jetty-servlet:9.3.7.RC1"
+  testCompile "org.eclipse.jetty:jetty-server:9.4.15.v20190215"
+  testCompile "org.eclipse.jetty:jetty-servlet:9.4.15.v20190215"
 }

gradle/wrapper/gradle-wrapper.properties
@@ -1,5 +1,5 @@
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-5.3.1-bin.zip
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists
-distributionUrl=https\://services.gradle.org/distributions/gradle-2.11-all.zip

commit 577ff4a516a439f0dcca7673553ce2147256d6de

Fix ISO-8601 date formatting
Was broken by 00d07252a0cf2fba1f08dcecf78b9add680a286f

gitlfs-common/src/main/java/ru/bozaro/gitlfs/common/JsonHelper.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
 
 import static com.fasterxml.jackson.core.util.DefaultPrettyPrinter.DEFAULT_ROOT_VALUE_SEPARATOR;
 
@@ -15,8 +17,17 @@ import static com.fasterxml.jackson.core.util.DefaultPrettyPrinter.DEFAULT_ROOT_
  * Json utility class.
  *
  * @author Artem V. Navrotskiy
+ * @author Marat Radchenko <marat@slonopootamus.org>
  */
 public final class JsonHelper {
+
+  /**
+   * git-lfs is broken and doesn't properly parse output of {@link com.fasterxml.jackson.databind.util.StdDateFormat}.
+   * <p/>
+   * See https://github.com/git-lfs/git-lfs/issues/3660
+   */
+  @NotNull
+  public static final DateFormat dateFormat = new ISO8601DateFormat();
 
   @NotNull
   public static final ObjectMapper mapper = new ObjectMapper();
@@ -25,7 +36,7 @@ public final class JsonHelper {
     mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     mapper.enable(SerializationFeature.INDENT_OUTPUT);
-    mapper.setDateFormat(new StdDateFormat());
+    mapper.setDateFormat(dateFormat);
 
     // By default, pretty printer uses system newline. Explicitly configure it to use \n
     mapper.setDefaultPrettyPrinter(

gitlfs-common/src/test/java/ru/bozaro/gitlfs/common/data/DateTest.java
@@ -1,0 +1,16 @@
+package ru.bozaro.gitlfs.common.data;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.bozaro.gitlfs.common.JsonHelper;
+
+import java.text.ParseException;
+
+public final class DateTest {
+
+  @Test
+  public void format() throws ParseException {
+    final String str = "2006-01-02T15:04:05Z";
+    Assert.assertEquals(JsonHelper.dateFormat.format(JsonHelper.dateFormat.parse(str)), str);
+  }
+}