f22dfcb122bd12f1fc83939a342af61d70589a40
Marat Radchenko <marat@slonopotamus.org>
2019-05-21 09:50:42 +0300

    Fix deprecated ISO8601DateFormat usage

--- gitlfs-common/src/main/java/ru/bozaro/gitlfs/common/JsonHelper.java
+++ gitlfs-common/src/main/java/ru/bozaro/gitlfs/common/JsonHelper.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DateFormat;
@@ -17,17 +17,17 @@ import static com.fasterxml.jackson.core.util.DefaultPrettyPrinter.DEFAULT_ROOT_
  * Json utility class.
  *
  * @author Artem V. Navrotskiy
- * @author Marat Radchenko <marat@slonopootamus.org>
+ * @author Marat Radchenko <marat@slonopotamus.org>
  */
 public final class JsonHelper {
 
   /**
-   * git-lfs is broken and doesn't properly parse output of {@link com.fasterxml.jackson.databind.util.StdDateFormat}.
+   * git-lfs cannot parse timezone without colon: {@link com.fasterxml.jackson.databind.util.StdDateFormat}.
    * <p/>
    * See https://github.com/git-lfs/git-lfs/issues/3660
    */
   @NotNull
-  public static final DateFormat dateFormat = new ISO8601DateFormat();
+  public static final DateFormat dateFormat = StdDateFormat.instance.withColonInTimeZone(true);
 
   @NotNull
   public static final ObjectMapper mapper = new ObjectMapper();
