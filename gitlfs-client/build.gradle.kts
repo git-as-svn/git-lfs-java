bd89fd936008b11d71878b688c2539a1871b44d6
Marat Radchenko <marat@slonopotamus.org>
2019-12-30 18:16:23 +0300

    Replace deprecated 'compile' dependency scope with 'implementation'

--- gitlfs-client/build.gradle.kts
+++ gitlfs-client/build.gradle.kts
@@ -1,10 +1,10 @@
 description = "Java Git-LFS client library"
 
 dependencies {
-    compile(project(":gitlfs-common"))
-    compile("org.apache.httpcomponents:httpclient:4.5.10")
-    compile("org.slf4j:slf4j-api:1.7.28")
+    api(project(":gitlfs-common"))
+    api("org.apache.httpcomponents:httpclient:4.5.10")
+    implementation("org.slf4j:slf4j-api:1.7.28")
 
-    testCompile("org.yaml:snakeyaml:1.25")
+    testImplementation("org.yaml:snakeyaml:1.25")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.28")
 }
