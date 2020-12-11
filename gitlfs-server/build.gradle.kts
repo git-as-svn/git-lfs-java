e025a22bc847d13ee149e54bc79d967b5ad613b5
Marat Radchenko <marat@slonopotamus.org>
2020-12-11 19:41:04 +0300

    Update dependencies

--- gitlfs-server/build.gradle.kts
+++ gitlfs-server/build.gradle.kts
@@ -5,6 +5,6 @@ dependencies {
     api("javax.servlet:javax.servlet-api:4.0.1")
 
     testImplementation(project(":gitlfs-client"))
-    testImplementation("org.eclipse.jetty:jetty-servlet:9.4.31.v20200723")
+    testImplementation("org.eclipse.jetty:jetty-servlet:9.4.35.v20201120")
     testRuntimeOnly("org.slf4j:slf4j-simple:1.7.30")
 }
