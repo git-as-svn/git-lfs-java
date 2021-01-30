5d2325cb45717b55c7b5532150716f9516bddad6
Marat Radchenko <marat@slonopotamus.org>
2021-01-30 16:55:20 +0300

    v0.17.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
 
 allprojects {
     group = "ru.bozaro.gitlfs"
-    version = "0.17.0-SNAPSHOT"
+    version = "0.17.0"
 
     apply(plugin = "idea")
     apply(plugin = "com.github.ben-manes.versions")
