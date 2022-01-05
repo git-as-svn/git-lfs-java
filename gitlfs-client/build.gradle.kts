d228bcd4fde7fa3fe84a859199e2a4180baf5622
Marat Radchenko <marat@slonopotamus.org>
2022-01-05 18:30:28 +0300

    Upgrade Gradle and deps

--- gitlfs-client/build.gradle.kts
+++ gitlfs-client/build.gradle.kts
@@ -5,6 +5,6 @@ dependencies {
     api("org.apache.httpcomponents:httpclient:4.5.13")
     implementation("org.slf4j:slf4j-api:2.0.0-alpha5")
 
-    testImplementation("org.yaml:snakeyaml:1.29")
+    testImplementation("org.yaml:snakeyaml:1.30")
     testRuntimeOnly("org.slf4j:slf4j-simple:2.0.0-alpha5")
 }
