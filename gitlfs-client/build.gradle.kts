805fb316626b2b2bab4b108cedfde9a33a4c52cb
Marat Radchenko <marat@slonopotamus.org>
2022-01-27 20:00:27 +0300

    Downgrade SLF4J to 1.7.x

--- gitlfs-client/build.gradle.kts
+++ gitlfs-client/build.gradle.kts
@@ -3,8 +3,8 @@ description = "Java Git-LFS client library"
 dependencies {
     api(project(":gitlfs-common"))
     api("org.apache.httpcomponents:httpclient:4.5.13")
-    implementation("org.slf4j:slf4j-api:2.0.0-alpha5")
+    implementation("org.slf4j:slf4j-api:1.7.35")
 
     testImplementation("org.yaml:snakeyaml:1.30")
-    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.0-alpha5")
+    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.35")
 }
--- gitlfs-server/build.gradle.kts
+++ gitlfs-server/build.gradle.kts
@@ -6,5 +6,5 @@ dependencies {
 
     testImplementation(project(":gitlfs-client"))
     testImplementation("org.eclipse.jetty:jetty-servlet:11.0.7")
-    testRuntimeOnly("org.slf4j:slf4j-simple:2.0.0-alpha5")
+    testRuntimeOnly("org.slf4j:slf4j-simple:1.7.32")
 }
